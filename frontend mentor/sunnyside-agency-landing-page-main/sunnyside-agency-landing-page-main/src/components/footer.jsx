d58981f1349d4c1d655e9e38ba6f2d288eba5708
OtabekToshtemirov <47772571+Otabek04@users.noreply.github.com>
2023-06-10 20:58:50 +0500

    complet

--- frontend mentor/sunnyside-agency-landing-page-main/sunnyside-agency-landing-page-main/src/components/footer.jsx
+++ frontend mentor/sunnyside-agency-landing-page-main/sunnyside-agency-landing-page-main/src/components/footer.jsx
@@ -1,8 +1,31 @@
+import logo from '../../images/logo.svg'
+import './footer.css'
+import facebook from '../../images/icon-facebook.svg'
+import pinterest from '../../images/icon-pinterest.svg'
+import instagram from '../../images/icon-instagram.svg'
+import twitter from '../../images/icon-twitter.svg'
+
+
 function Footer() {
     return (
         <footer className="footer">
             <div className="container">
-                <span className="text-muted">Place sticky footer content here.</span>
+                <div className="footer-logo">
+                    <img src={logo} alt=""/>
+                </div>
+                <div className="footer-menu">
+                    <ul>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contacts</a></li>
+                    </ul>
+                </div>
+                <div className="footer-icons">
+                    <img src={facebook} alt=""/>
+                    <img src={instagram} alt=""/>
+                    <img src={twitter} alt=""/>
+                    <img src={pinterest} alt=""/>
+                </div>
             </div>
         </footer>
     );
--- frontend mentor/sunnyside-agency-landing-page-main/sunnyside-agency-landing-page-main/src/components/main.jsx
+++ frontend mentor/sunnyside-agency-landing-page-main/sunnyside-agency-landing-page-main/src/components/main.jsx
@@ -1,7 +1,13 @@
 import egg from '../../images/desktop/image-transform.jpg';
 import cup from '../../images/desktop/image-stand-out.jpg';
+import emily from '../../images/image-emily.jpg'
+import thomas from '../../images/image-thomas.jpg'
+import jennie from '../../images/image-jennie.jpg'
 import './main.css';
-
+import bottles from '../../images/desktop/image-gallery-milkbottles.jpg'  ;
+import cone from '../../images/desktop/image-gallery-cone.jpg'  ;
+import orange from '../../images/desktop/image-gallery-orange.jpg'  ;
+import sugar from '../../images/desktop/image-gallery-sugarcubes.jpg'  ;
 
 function Main() {
     return (
@@ -34,12 +40,91 @@ function Main() {
                             Stand out to the right audience
                         </h2>
                         <p className="section_2-paragraph">
-                            Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.
+                            Using a collaborative formula of designers, researchers, photographers, videographers, and
+                            copywriters, we’ll build and extend your brand in digital places.
                         </p>
-                    </div>  
+                        <button className="section_1-btn">
+                            Learn more
+                        </button>
+                    </div>
 
                 </section>
+                <section className=" section_3">
+                    <div className=" section_3-img1">
+                        <div className="padding">
+                            <h2>
+                                Graphic design
+                            </h2>
+                            <p>
+                                Great design makes you memorable. We deliver artwork that underscores your brand message
+                                and captures potential clients’ attention.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="section_3-img2">
+                        <div className="padding">
+                            <h2>
+                                Photography
+                            </h2>
+                            <p>
+                                Increase your credibility by getting the most stunning, high-quality photos that improve
+                                your business image.
+                            </p>
+                        </div>
 
+
+                    </div>
+                </section>
+                {/*    reviews----------------------------*/}
+                <div className="reviews">
+                    <h1 className="title">
+                        CLIENT TESTIMONIALS
+                    </h1>
+                    <div className="row">
+                        <div className="col">
+                            <img src={emily} alt=""/>
+                            <p>
+                                We put our trust in Sunnyside and they delivered, making sure our needs were met and
+                                deadlines were always hit.
+                            </p>
+                            <h4>
+                                <b> Emily R.</b>
+                            </h4>
+                            <span>Marketing Director</span>
+                        </div>
+                        <div className="col">
+                            <img src={thomas} alt=""/>
+                            <p>
+                                {/* eslint-disable-next-line react/no-unescaped-entities */}
+                                Sunnyside enthusiasm coupled with their keen interest in our brand's success made it a satisfying and enjoyable experience.
+                            </p>
+                            <h4>
+                                <b> Thomas S.</b>
+                            </h4>
+                            <span>Chief Operating Officer</span>
+                        </div>
+                        <div className="col">
+                            <img src={jennie} alt=""/>
+                            <p>
+                                Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!
+                            </p>
+                            <h4>
+                                <b> Jennie F.</b>
+
+                            </h4>
+                            <span>Business Owner</span>
+                        </div>
+
+
+                    </div>
+                </div>
+            {/*    images-------------------------------*/}
+                <div className="images">
+                    <img src={bottles} alt=""/>
+                    <img src={orange} alt=""/>
+                    <img src={cone} alt=""/>
+                    <img src={sugar} alt=""/>
+                </div>
 
 
             </div>
